9e34935e4bf7e898daf489fae8dec82a5059696f
marcus.conduru <marcus.conduru@gmail.com>
2022-06-10 01:00:03 -0300

    feat: ensure completed main page

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+$("#header").load("/Presentation/component/header/header.html"); 
+$("#footer").load("/Presentation/component/footer/footer.html"); 
+$('.main__gallery').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+})
