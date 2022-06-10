21bc7adcbe099b4abae4ec05c358ad9610dff5df
marcus.conduru <marcus.conduru@gmail.com>
2022-06-10 20:19:07 -0300

    feat: add new component spinner loader

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 $("#header").load("/Presentation/component/header/header.html"); 
 $("#footer").load("/Presentation/component/footer/footer.html"); 
+$("#spinner").load("/Presentation/component/loading/loading.html"); 
 $('.main__gallery').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
